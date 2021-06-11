858953bd6dbde6a3c749ae27cf8685bb1591f4f1
konradDlugosz <1812755@brunel.ac.uk>
2021-06-11 23:34:07 +0100

    Initial Commit

---
+++ CardGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CardGame
+//
+//  Created by Konrad Dlugosz on 11/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
